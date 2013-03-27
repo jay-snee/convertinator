f3a2a92f8f0dfa35dc0da56866f19075390e2845
Jay Snee <jay.snee@me.com>
2013-03-27 15:02:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caroline::Application.config.secret_token = '4e934667ce17015866c6eba06e0e35ed3dd32c562440a4e9f930a8b8439f48b8a68969ad358d8adbc95a6c85dafec69bc14165469e3a8dae021666b19d29752b'
